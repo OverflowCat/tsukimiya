0eb2c080e6419c076a8f3458c67cef999e7c6c8f
OverflowCat <overflowcat@gmail.com>
2024-12-10 00:07:02 +0800

    feat: eval main

--- src/bot.js
+++ src/bot.js
@@ -6,20 +6,28 @@ const bot = new Bot(token);
 
 
 function splitCmd(str) {
-	    const index = str.search(/\s/);
-	    if (index !== -1) {
-		            const firstPart = str.slice(0, index);
-		            const secondPart = str.slice(index + 1); 
-		            return [firstPart, secondPart]; 
-		        }
-	    return [str, ''];
+    const index = str.search(/\s/);
+    if (index !== -1) {
+        const firstPart = str.slice(0, index);
+        const secondPart = str.slice(index + 1);
+        return [firstPart, secondPart];
+    }
+    return [str, ''];
 }
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.command("eval", async (ctx) => {
     const text = ctx.message.text ?? ctx.message.caption ?? "";
-    const code = splitCmd(text).at(-1) ?? ""; //text.split(" ").slice(1).join(" ");
+    let code = splitCmd(text).at(-1)?.trim() ?? ""; //text.split(" ").slice(1).join(" ");
     console.info(text);
+    if (!/fn +main/.test(code)) {
+        lines = code.split("\n");
+        const last_line = lines.at(-1);
+        if (!/println/.test(last_line)) {
+            lines[lines.length - 1] = `println(${last_line})`;
+        }
+        code = `fn main { ${lines.join("\n")} }`;
+    }
     const result = await run(code);
     ctx.reply(result);
 });
